f019b5436b2df6d99a831e93f427cde4447353f5
YellapuNeerajKumar <yellapuneerajkumar01@gmail.com>
2024-04-14 07:51:22 +0000

    first commit

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,29 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class Welcome extends Component {
+  state = {subsribedStatus: false}
+
+  isSubscribed = () => {
+    const {subsribedStatus} = this.state
+    this.setState({subsribedStatus: !subsribedStatus})
+  }
+
+  render() {
+    const {subsribedStatus} = this.state
+    return (
+      <div className="CardContainer">
+        <h1>Welcome</h1>
+        <p>Thank You! Happy Learning</p>
+        {subsribedStatus ? (
+          <button onClick={this.isSubscribed}>Subscribed</button>
+        ) : (
+          <button onClick={this.isSubscribed}>Subscribe</button>
+        )}
+      </div>
+    )
+  }
+}
+
+export default Welcome
